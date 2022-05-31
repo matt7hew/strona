3077d4d245c922da1ef836fa9c61f63affbecdd1
Mateusz <mati05091997@gmail.com>
2022-05-31 21:45:09 +0200

    Init.

---
+++ js/main.js
@@ -0,0 +1,53 @@
+const nav = document.querySelector('.nav__items');
+const navBtn = document.querySelector('.burger-btn');
+const allNavItems = document.querySelectorAll('.nav__item');
+const navBtnBars = document.querySelector('.burger-btn__bars');
+const allSections = document.querySelectorAll('.section');
+const footerYear = document.querySelector('.footer__year');
+
+const handleNav = () => {
+	nav.classList.toggle('nav__items--active');
+
+	allNavItems.forEach((item) => {
+		item.addEventListener('click', () => {
+			nav.classList.remove('nav__items--active');
+			allNavItems.classList.remove('nav-items-animation');
+		});
+	});
+	handleNavItemsAnimation();
+};
+
+const handleNavItemsAnimation = () => {
+	let delayTime = 0;
+
+	allNavItems.forEach((item) => {
+		item.classList.toggle('nav-items-animation');
+		item.style.animationDelay = '.' + delayTime + 's';
+		delayTime++;
+	});
+};
+
+const handleObserver = () => {
+	const currentSection = window.scrollY;
+
+	allSections.forEach((section) => {
+		if (
+			section.classList.contains('white-section') &&
+			section.offsetTop <= currentSection + 60
+		) {
+			navBtnBars.classList.add('black-bars-color');
+		} else if (!section.classList.contains('white-section') &&
+			section.offsetTop <= currentSection + 60
+		) {
+			navBtnBars.classList.remove('black-bars-color');
+		}
+	});
+};
+
+const handleCurrentYear = () => {
+	const year = new Date().getFullYear();
+	footerYear.innerText = year;
+};
+
+navBtn.addEventListener('click', handleNav);
+window.addEventListener('scroll', handleObserver);
